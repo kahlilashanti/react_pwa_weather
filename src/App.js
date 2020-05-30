6e86c24e434aeb471b05f0a39594f0f95a2cdada
seetsdev <bushongo@protonmail.com>
2020-05-29 20:45:53 -0700

    finished react functionality

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 
 const App = () => {
     const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
 
     const search = async (e) => {
         if (e.key === 'Enter') {
-            const data = await fetchWeather(query)
+            const data = await fetchWeather(query);
 
             console.log(data);
+            setWeather(data);
+            //after we hit enter we want to reset the query
+            setQuery('');
         }
     }
 
@@ -26,6 +30,23 @@ const App = () => {
                 onKeyPress={search}
 
             />>
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                        <sup>{weather.timezone}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
